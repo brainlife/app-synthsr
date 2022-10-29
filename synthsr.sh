88a09acdab760a31daa3b1e98780b0753bf6e322
Brad Caron <bacaron@utexas.edu>
2022-10-28 21:31:20 -0500

    reason: update with thread count

--- synthsr.sh
+++ synthsr.sh
@@ -7,6 +7,6 @@ ncores=4
 [ ! -d output ] && mkdir -p output && outdir="./output"
 
 [ ! -f ${outdir}/${TYPE}.nii.gz ] && echo "running SynthSR" && \
-	mri_synthsr --i ${input} --o ${outdir}/${TYPE}.nii.gz
+	mri_synthsr --i ${input} --o ${outdir}/${TYPE}.nii.gz --threads ${ncores}
 
 [ ! -f ${outdir}/${TYPE}.nii.gz ] && echo "something went wrong. check logs" && exit 1 || echo "complete" && exit 0
