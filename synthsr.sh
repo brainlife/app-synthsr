3b5273eacf105bae59b915e6d1b993672164f9e8
Brad Caron <bacaron245@gmail.com>
2022-11-10 22:21:14 -0600

    Update synthsr.sh

--- synthsr.sh
+++ synthsr.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-input=`jq -r '.input' config.json`
-TYPE=`jq -r '.type' config.json`
+t1=`jq -r '.t1' config.json`
+t2=`jq -r '.t2' config.json`
 ncores=4
 
 [ ! -d output ] && mkdir -p output && outdir="./output"
 
-[ ! -f ${outdir}/${TYPE}.nii.gz ] && echo "running SynthSR" && \
-	mri_synthsr --i ${input} --o ${outdir}/${TYPE}.nii.gz --threads ${ncores}
+[ ! -f ${outdir}/${TYPE}.nii.gz ] && echo "running SynthSR Hyperfine" && \
+	mri_synthsr_hyperfine --t1 ${t1} --t2 ${t2} --o ${outdir}/t1.nii.gz --threads ${ncores}
 
 [ ! -f ${outdir}/${TYPE}.nii.gz ] && echo "something went wrong. check logs" && exit 1 || echo "complete" && exit 0
